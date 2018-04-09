696b340434b990d6f2374bf2cf15a2379f4e511e
xsq <xsqrenda@qq.com>
2018-04-09 22:11:04 +0800

    加入json文件读取模块

--- python/control/comment_control.py
+++ python/control/comment_control.py
@@ -7,9 +7,35 @@
 @describe:
 """
 from web import app
-from flask import render_template
+from python.control.file_handle import read_json_file
+from flask import render_template, jsonify
 
 print('control comment')
+
+dirname = 'C:/Users/Administrator/AppData/Roaming/IQIYI Video/LStyle/vmPage/download/search/zipCache'
+filename = ['index.json']
+
+
+@app.route('/api/v1/get')
+def get():
+    newfilename = dirname+'/'+filename[0]
+    jl = read_json_file(newfilename)
+    # print(jl)
+    # with 的用法：with jl['data']不可用
+    # with jl['data'] as jldata:
+    # with jl['data']:
+    #     for jd in jl['data']:
+    #         print(jd['query'])
+    if 'data' in jl.keys():
+        for jd in jl['data']:
+            if 'query' in jl['data'][0].keys():
+                print(jd['query'])
+    # print(len(jl))
+    # for
+
+    # print(jl['data'][0].keys())
+    # print(jl['data'][0]['query'])
+    return jsonify(jl)
 
 
 @app.route('/comment/add')
@@ -20,4 +46,3 @@ def comment_add():
 @app.route('/comment/delete')
 def comment_delete():
     return 'delete comment'
-
