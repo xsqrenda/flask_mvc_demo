e456eb1995353e02ff3930cff6794bf5a261cc8e
xsqrenda <xsqrenda@qq.com>
2019-02-12 18:12:53 +0800

    增加雁滩北两室整租爬取功能

--- python/control/comment_control.py
+++ python/control/comment_control.py
@@ -9,11 +9,28 @@
 from web import app
 from python.control.file_handle import read_json_file
 from flask import render_template, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+
 
 print('control comment')
 
 dirname = 'C:/Users/Administrator/AppData/Roaming/IQIYI Video/LStyle/vmPage/download/search/zipCache'
 filename = ['index.json']
+
+@app.route('/')
+def index(name=None):
+    price = []
+    url = "https://lz.zu.fang.com/house-a0469-b014457/g22-n31-s31/"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "lxml")
+    houseList = soup.select('div.houseList')
+    house = houseList[0].find_all('dl')
+    for i in house:
+        print(i.select('span.price')[0].text)
+        price.append(i.select('span.price')[0].text)
+    return render_template('index.html', name=name,price=price)
 
 
 @app.route('/api/v1/get')
